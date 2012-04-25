13545109aad898a131ad0d96541dde818228dbea
Arne Redlich <arne.redlich@googlemail.com>
2012-04-25 23:48:16 +0200

    Examples/Stdin.hs: fix compiler warning
    Stdin.hs:1:8:
        Warning: In the use of `catch'
                 (imported from Prelude, but defined in System.IO.Error):
                 Deprecated: "Please use the new exceptions variant, Control.Exception.catch"
    
    Signed-off-by: Arne Redlich <arne.redlich@googlemail.com>

--- examples/Stdin.hs
+++ examples/Stdin.hs
@@ -1,11 +1,13 @@
 module Main where
 
+import Prelude hiding (catch)
 import System.Linux.Epoll
 import System.Posix.IO
 import System.IO
 import Data.Maybe
 import Text.Printf
 import Control.Concurrent.MVar
+import Control.Exception (catch, SomeException)
 
 eventTypes :: EventType
 eventTypes = combineEvents [inEvent, edgeTriggeredEvent, oneShotEvent]
@@ -15,7 +17,8 @@ callback lck d e = do
     let et = eventType e
         fd = eventFd e
     printf "event=%s, fd=%s\n" (show et) (show fd)
-    (s, c) <- fdRead fd 16 `catch` \er -> print er >> return ("", 0)
+    (s, c) <-
+      fdRead fd 16 `catch` \er -> print (er :: SomeException) >> return ("", 0)
     printf "read %s byte(s): %s\n" (show c) s
     if s == "quit\n"
         then putMVar lck ()
